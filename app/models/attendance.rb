cb7187c9882ae3374a8c06231630726d9782a168
jubin404 <jubin.jose@terrificminds.com>
2023-10-30 12:37:29 +0530

    refactor attendance model and spec file

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,14 @@
 class Attendance < ApplicationRecord
 
   belongs_to :employee
+  validates :date, presence: true
+  validate :punch_out_time_must_be_greater_than_punch_in_time
+
+  def punch_out_time_must_be_greater_than_punch_in_time
+    return if punch_in_time.nil? || punch_out_time.nil?
+
+    if punch_out_time <= punch_in_time
+      errors.add(:punch_out_time, "must be greater than punch in time")
+    end
+  end
 end
--- spec/attendance_spec.rb
+++ spec/attendance_spec.rb
@@ -1,13 +1,28 @@
 require 'rails_helper'
 
 describe Attendance do
-    it 'Punch out time must be after punch in time' do
-      attendance = Attendance.create(date: '01-01-2023', punch_in_time: '10:00 am', punch_out_time: '6:00 pm')
-      expect(attendance[:punch_out_time]).to be >= attendance[:punch_in_time]
-    end
+  before do
+    @address= Address.create(line_one: 'home')
+    @gender= Gender.create(title: 'male')
+    @employee= Employee.create(email: 'test@gmail.com', 
+                            password: '123456', 
+                            address_id: @address[:id],
+                            first_name: 'first',
+                            last_name: 'last', 
+                            gender_id: @gender[:id],
+                            employee_id: 'TM123')
+    @attendance = Attendance.new(employee_id: @employee[:id], date: '01-01-2023', punch_in_time: '10:00 am', punch_out_time: '6:00 pm')
+    expect(@attendance.save).to eq true
+  end
 
-    it 'Attendance entry must have a valid date' do
-      attendance = Attendance.create(date: '31-02-2023')
-      expect(attendance.valid?).to eq false
-    end
+  it 'Punch out time must be after punch in time' do
+    @attendance.punch_in_time = '6:01 pm'
+  end
+  it 'Attendance entry must have a valid date' do
+    @attendance.date = '31-02-2023'
+  end
+  
+  after do
+    expect(@attendance.save).to eq false
+  end
 end
