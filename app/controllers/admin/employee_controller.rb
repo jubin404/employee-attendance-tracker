9a170523aff21fe0b7c7e7b69e8300fc452ec820
jubin404 <jubin.jose@terrificminds.com>
2023-11-08 15:14:55 +0530

    make admin side  employee creation dynamic

--- app/controllers/admin/employee_controller.rb
+++ app/controllers/admin/employee_controller.rb
@@ -1,4 +1,4 @@
-class Admin::EmployeeController < ApplicationController
+class Admin::EmployeeController < Admin::BaseController
   
   def index
   end
@@ -7,12 +7,45 @@ class Admin::EmployeeController < ApplicationController
   end
 
   def new
+    @employee = Employee.new
     respond_to do |format|
       format.html
       format.js
     end
   end
 
+  def create    
+    @employee = Employee.new(employee_params)
+    @employee[:company_id] = employee_company_id
+    if @employee.save
+      save_employee_address
+      flash[:notice] = "Employee was added successfully."
+      redirect_to admin_employee_index_path
+    else
+      flash[:notice] = "Something went wrong"
+      render 'new'
+    end
+  end
+  
   def show
+  end
+  
+  def employee_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :phone_number, :password, :gender_id)
+  end
+
+  def address_params
+    params.require(:employee).permit(:line_one, :line_two, :city, :pin, :country)
+  end
+
+  def save_employee_address
+    @address = Address.new(address_params)
+    @address[:employee_id] = @employee[:id]
+    @address[:status] = 'active'
+    @address.save
+  end
+
+  def employee_company_id
+    'TM101'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   namespace :admin do
     get 'dashboard', to: 'dashboard#index'
     resources :employee
+    post 'employee/new', to: 'employee#create'
     resources :attendance
   end
 end
