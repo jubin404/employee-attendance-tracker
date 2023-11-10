b32043dddca9b2047e2b24599e2f26e75d80a174
jubin404 <jubin.jose@terrificminds.com>
2023-11-10 14:44:42 +0530

    refactor attendance addition from admin side

--- app/controllers/admin/attendance_controller.rb
+++ app/controllers/admin/attendance_controller.rb
@@ -6,11 +6,14 @@ class Admin::AttendanceController < ApplicationController
 
   def new
     @attendance = Attendance.new
-    @attendance_params = employee_attendance_params
+    @attendance_params = employee_attendance_params if adding_employee_attendance
   end
 
   def create
     @attendance = Attendance.new(attendance_params)
+    @attendance[:status] = 'active'
+    employee_from_company_id
+
     if @attendance.save
       flash[:notice] = "Attendance was added successfully."
       redirect_to admin_attendance_index_path
@@ -23,10 +26,18 @@ class Admin::AttendanceController < ApplicationController
   private
 
   def attendance_params 
-    params.require(:attendance).permit(:employee_id, :attendance_status, :date, :punch_in_time, :punch_out_time, :status)
+    params.require(:attendance).permit(:employee_id, :attendance_status, :date, :punch_in_time, :punch_out_time)
   end
 
   def employee_attendance_params
     params.require(:attendance).permit(:employee_id, :employee_name)
+  end
+
+  def adding_employee_attendance
+    params.include?(:attendance)
+  end
+
+  def employee_from_company_id
+    @attendance[:employee_id] = Employee.find_by(company_id: attendance_params[:employee_id])[:id]
   end
 end
