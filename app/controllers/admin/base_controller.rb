175a5233455bf386836732c4a8c9f00259747b67
jubin404 <jubin.jose@terrificminds.com>
2023-11-14 13:59:10 +0530

    update header navbar profile section to display only upon logging in

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -6,7 +6,6 @@ class Admin::BaseController < ApplicationController
 
   def authenticate_admin
     unless current_admin
-      flash[:alert] = 'You are not authorized to access this page. Please log in as an admin.'
       redirect_to new_admin_session_path
     end
   end
