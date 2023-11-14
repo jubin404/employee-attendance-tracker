03f74069496fe2d20d2c863eb8fdbfa104542e5a
jubin404 <jubin.jose@terrificminds.com>
2023-11-14 14:46:44 +0530

    remove registrable from employee and admin devise models

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
 
   validates :first_name, length: { minimum: 3 }
