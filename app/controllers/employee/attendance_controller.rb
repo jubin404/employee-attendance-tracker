6d9944ab7c1a8e4829633a548187976fd666a026
jubin404 <jubin.jose@terrificminds.com>
2023-11-17 14:33:05 +0530

    add filter by dates to employee side slists and redo some styling

--- app/controllers/employee/attendance_controller.rb
+++ app/controllers/employee/attendance_controller.rb
@@ -4,6 +4,13 @@ class Employee::AttendanceController < Employee::BaseController
   def index
     @attendance = Attendance.new
     @attendances = current_employee.attendances.page(params[:page]).order(created_at: :desc)
+
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+
+      @attendances = @attendances.between_dates(start_date, end_date).page(params[:page]).order(created_at: :desc)
+    end
   end
 
   def new
