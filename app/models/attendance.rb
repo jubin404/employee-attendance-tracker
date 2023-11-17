1ad2a71d5334ba472d6618dbb131aed3919010d7
jubin404 <jubin.jose@terrificminds.com>
2023-11-17 19:28:15 +0530

    add more validations to models

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,10 +3,14 @@ class Attendance < ApplicationRecord
   belongs_to :employee
 
   validates :date, presence: true
+  validates :attendance_status, presence: true
+  validates_uniqueness_of :date, scope: :employee_id, message: ': Attendance has already been marked for this date.'
+
   validate :punch_out_time_must_be_greater_than_punch_in_time
   validate :attendance_status_valid_value
+  validate :date_is_not_in_future
+  validate :time_check_for_status
   before_save :downcase_attendance_status
-
 
   scope :between_dates, ->(start_date, end_date) { where(date: start_date..end_date) }
   scope :today, -> { where(date: Date.today) }
@@ -20,13 +24,23 @@ class Attendance < ApplicationRecord
     end
   end
 
+  def date_is_not_in_future
+    errors.add(:date, 'is in future') if date > Date.today
+  end
+
+  def time_check_for_status
+    if attendance_status.strip.downcase == 'absent'
+      errors.add(:attendance_status, 'absent cannot take punching times') unless punch_in_time.nil? || punch_out_time.nil?
+    else
+      errors.add(:attendance_status, 'present must have punching times') if punch_in_time.nil? || punch_out_time.nil?
+    end 
+  end
+
   def downcase_attendance_status
-    self.attendance_status.downcase!
+    self.attendance_status.strip!.downcase!
  end
 
   def attendance_status_valid_value
-    return if attendance_status.nil?
-
     valid_values = ['present', 'absent']
     errors.add(:attendance_status, 'is not a valid value') unless valid_values.include?(attendance_status.downcase)
   end
